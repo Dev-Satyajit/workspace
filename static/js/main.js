738a54f6d31e23f225e98c2f93b9df8feac0f3c2
Satyajit Sinha <90083670+satyajitsinha1919@users.noreply.github.com>
2021-11-08 13:08:37 +0530

    Update

--- static/js/main.js
+++ static/js/main.js
@@ -1,12 +1,12 @@
 $(".btn-tools").click(function () {
-  $(this).css("color", "blueviolet");
+  $(this).css("color", "var(--main)");
   $(".btn-sheets").css("color", "#555");
   $("#tool-menu").addClass("active");
   $("#sheet-menu").removeClass("active");
   $(".tg").css("left", "50%");
 });
 $(".btn-sheets").click(function () {
-  $(this).css("color", "blueviolet");
+  $(this).css("color", "var(--main)");
   $(".btn-tools").css("color", "#555");
   $("#sheet-menu").addClass("active");
   $("#tool-menu").removeClass("active");
